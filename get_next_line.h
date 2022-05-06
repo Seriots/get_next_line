c1e98155cd0cb9d7fb857f2f081374a25a3712fb
Seriots <96475943+Seriots@users.noreply.github.com>
2022-05-06 17:03:51 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 09:24:20 by lgiband           #+#    #+#             */
+/*   Updated: 2022/05/05 10:49:45 by lgiband          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+# endif 
+# include <stdlib.h>
+# include <unistd.h>
+
+/*get_next_line.c*/
+char	*get_next_line(int fd);
+char	*ft_get_line(char *prev_read);
+char	*ft_cut_last_read(char *prev_read);
+char	*ft_read(int fd, char *prev_read);
+
+/*get_next_line_utils.c*/
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin2(char *s1, char *s2, char *result, size_t size1);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiband <lgiband@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 15:47:03 by lgiband           #+#    #+#             */
+/*   Updated: 2022/05/05 10:55:27 by lgiband          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(s + i))
+		i ++;
+	return (i);
+}
+
+char	*ft_strjoin2(char *s1, char *s2, char *result, size_t s1_size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1_size && s1[i])
+	{
+		result[i] = s1[i];
+		i ++;
+	}
+	while (s2[j])
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	return (result);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	s1_size;
+	size_t	s2_size;
+
+	if (s1 == 0)
+		s1_size = 0;
+	else
+		s1_size = ft_strlen(s1);
+	s2_size = ft_strlen(s2);
+	result = malloc(sizeof(char) * (s1_size + s2_size + 1));
+	if (!result)
+		return (0);
+	result[s1_size + s2_size] = 0;
+	result = ft_strjoin2(s1, s2, result, s1_size);
+	free(s1);
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (*(s + i))
+	{
+		if ((*(s + i) == (char)c))
+		{
+			ptr = (char *)s + i;
+			return (ptr);
+		}
+		i ++;
+	}
+	if (*(s + i) == (char)c)
+	{
+		ptr = (char *)s + i;
+		return (ptr);
+	}
+	return (0);
+}
